b5592dca80d766018cdfa9401dcb4eab4d650852
Noah Wolfe <Noah Wolfe>
2016-07-17 16:04:14 -0400

    Messing around with multiple classes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,26 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Tester aTest = new Tester(22, 33, 44);
+        System.out.println(aTest.giveANumber());
+    }
+}
+
+class Tester {
+    private int x;
+    private int y;
+    private int z;
+
+    Tester(int xVal, int yVal, int zVal) {
+        x = xVal;
+        y = yVal;
+        z = zVal;
+        System.out.println("X:" + x);
+        System.out.println("Y:" + y);
+        System.out.println("Z:" + z);
+    }
+
+    public int giveANumber() {
+        return x + y + z;
     }
 }
